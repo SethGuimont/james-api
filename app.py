8d6eb128dcbf5fa8af904c9f65620876d01d9707
Seth Guimont <sethguimont@outlook.com>
2023-09-04 10:42:32 -0500

    separated out queries and pages for different menus

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from json2html import *
 from constants import *
+
 load_dotenv()
 
 app = Flask(__name__)
@@ -15,27 +16,91 @@ connection = psycopg2.connect(url)
 '''Begin routes for HTML templates'''
 
 
+# informational static pages
 @app.route("/")
 def home():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/location")
+def location():
+    return render_template("location.html")
+
+
+@app.route("/partners")
+def partners():
+    return render_template("partners.html")
+
+
+# Pages with dynamic data
+@app.route("/bakery")
+def bakery():
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(SELECT_ALL_POSTS)
-            menu_items = cursor.fetchall()
+            cursor.execute(SELECT_ALL_BAKERY)
+            bakery = cursor.fetchall()
             cursor.close()
-    return render_template("index.html", menu_items=menu_items)
+    return render_template("bakery.html", menu_items=bakery)
+
+
+@app.route("/breakfast")
+def breakfast():
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(SELECT_ALL_BREAKFAST)
+            breakfast = cursor.fetchall()
+            cursor.close()
+    return render_template("breakfast.html", menu_items=breakfast)
+
+
+@app.route("/lunch")
+def lunch():
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(SELECT_ALL_LUNCH)
+            lunch = cursor.fetchall()
+            cursor.close()
+    return render_template("lunch.html", menu_items=lunch)
+
+
+@app.route("/dinner")
+def dinner():
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(SELECT_ALL_DINNER)
+            dinner = cursor.fetchall()
+            cursor.close()
+    return render_template("dinner.html", menu_items=dinner)
+
+
+@app.route("/dessert")
+def dessert():
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(SELECT_ALL_DESSERT)
+            dessert = cursor.fetchall()
+            cursor.close()
+    return render_template("dessert.html", menu_items=dessert)
 
 
 '''Begin API Functionality'''
 
 
+#  Lunch and dinner
 @app.route("/api/menuitems", methods=["POST"])
 def create_menuitem():
     data = request.get_json()
     name = data["name"]
     description = data["description"]
+    tag = data["tag"]
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(INSERT_MENUITEM_RETURN_ID, (name, description,))
+            cursor.execute(INSERT_MENUITEM_RETURN_ID, (name, description, tag))
             id = cursor.fetchone()[0]
     return {"id": id, "name": name, "message": f"menu item {name} created."}, 201
 
@@ -44,12 +109,12 @@ def create_menuitem():
 def get_all_users():
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(SELECT_ALL_POSTS)
+            cursor.execute(SELECT_ALL_ITEMS)
             menu_items = cursor.fetchall()
             if menu_items:
                 result = []
                 for item in menu_items:
-                    result.append({"name": item[1], "description": item[2]})
+                    result.append({"id": item[0], "name": item[1], "description": item[2], "tag": item[3]})
                 return jsonify(result)
             else:
                 return jsonify({"error": f"Users not found."}), 404
@@ -65,5 +130,13 @@ def delete_menuitem(id):
         return jsonify({"message": f"menu item with ID {id} deleted."})
 
 
+# Breakfast
+
+
+# Bakery
+
 if __name__ == "__main__":
     app.run()
+
+# Have menu pop up with QR reader
+# url is https://fluffy-cat-bb1787883c94.herokuapp.com/
