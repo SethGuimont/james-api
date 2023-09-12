a228b8518a610e1b3776461f6279a38d793a5b4d
Seth Guimont <sethguimont@outlook.com>
2023-09-11 19:50:35 -0500

    added more unit tests

--- tests/test_all.py
+++ tests/test_all.py
@@ -25,6 +25,30 @@ def test_landing(client):
     assert landing.status_code == 200
 
 
+def test_about(client):
+    about_page = client.get("/about")
+    html = about_page.data.decode()
+
+    assert "Fluffy Cat Pizza" in html
+    assert about_page.status_code == 200
+
+
+def test_location(client):
+    location_page = client.get("/location")
+    html = location_page.data.decode()
+
+    assert "Fluffy Cat Pizza" in html
+    assert location_page.status_code == 200
+
+
+def test_partners(client):
+    partners_page = client.get("/partners")
+    html = partners_page.data.decode()
+
+    assert "Fluffy Cat Pizza" in html
+    assert partners_page.status_code == 200
+
+
 def test_bakery(client):
     bakery_page = client.get("/bakery")
     html = bakery_page.data.decode()
@@ -65,6 +89,29 @@ def test_dessert(client):
     assert dessert_page.status_code == 200
 
 
+def test_employee(client):
+    dessert_page = client.get("/employee")
+    html = dessert_page.data.decode()
+
+    assert "Log in" in html
+    assert dessert_page.status_code == 200
+
+
+def test_404(client):
+    not_found_page = client.get("/4654654654654")
+    html = not_found_page.data.decode()
+
+    assert "Oops!" in html
+    assert not_found_page.status_code == 200
+
+
+# Retrieving menu items should return a 200
 def test_apiget(client):
     apiget = client.get("/api/menuitems")
     assert apiget.status_code == 200
+
+
+# Invalid menu item to delete returns 404
+def test_apidelete(client):
+    apidelete = client.delete("/api/menuitems/1000")
+    assert apidelete.status_code == 404
